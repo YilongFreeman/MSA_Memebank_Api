722a71c9f9d8d0261b310d20fe87507a303ec204
YilongFreeman <leeddeng@hotmail.com>
2018-11-19 22:40:48 +1300

    Memebank Done

---
+++ Data/MemebankContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Memebank.Models
+{
+    public class MemebankContext : DbContext
+    {
+        public MemebankContext (DbContextOptions<MemebankContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Memebank.Models.MemeItem> MemeItem { get; set; }
+    }
+}
